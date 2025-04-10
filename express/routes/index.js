3d189a917599f3bbac0565c7b28e83df3e01c171
Rick Michael <rickmikegull@icloud.com>
2025-04-10 11:31:13 -0700

    add core files

---
+++ express/routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  const hello = "hello from routes/index.js";
+  console.log(hello);
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
