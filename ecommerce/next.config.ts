6e17e76bab9cdcc5a98b7a59556aa3000b5382bc
Rick Michael <rickmikegull@icloud.com>
2025-04-26 14:49:09 -0700

    modifiy next config for pages

--- ecommerce/next.config.ts
+++ ecommerce/next.config.ts
@@ -4,17 +4,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-interface Config {
-    port: number;
-    nodeEnv: string;
-}
-
-const config: Config = {
-    port: Number(process.env.PORT) || 3000,
-    nodeEnv: process.env.NODE_ENV || 'development',
-};
 
 const nextConfig: NextConfig = {
+    port: Number(process.env.PORT) || 3000,
+    nodeEnv: process.env.NODE_ENV || 'development',
+    output: 'export',
   /* config options here */
 };
 
